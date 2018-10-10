122189a4cd66f029a3971100db11cb67331ec4b7
Aleksandr Nedorezov <n.sasha@list.ru>
2018-10-10 22:08:17 +0400

    dev commit

---
+++ testXcode941/Dev Folder/TXDevVC.h
@@ -0,0 +1,13 @@
+//
+//  TXDevVC.h
+//  testXcode941
+//
+//  Created by Aleksandr Nedorezov on 10.10.2018.
+//  Copyright © 2018 Aleksandr Nedorezov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TXDevVC : UIViewController
+
+@end
---
+++ testXcode941/Dev Folder/TXDevVC.m
@@ -0,0 +1,37 @@
+//
+//  TXDevVC.m
+//  testXcode941
+//
+//  Created by Aleksandr Nedorezov on 10.10.2018.
+//  Copyright © 2018 Aleksandr Nedorezov. All rights reserved.
+//
+
+#import "TXDevVC.h"
+
+@interface TXDevVC ()
+
+@end
+
+@implementation TXDevVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
