e4d9d5c0f682f56ccec5ad2d3670407b0ece3db9
Aleksandr Nedorezov <n.sasha@list.ru>
2018-10-16 23:47:28 +0400

    Future21-3

---
+++ testXcode941/Dev2/Future21/future21-3.m
@@ -0,0 +1,13 @@
+//
+//  future21-3.m
+//  testXcode941
+//
+//  Created by Aleksandr Nedorezov on 16.10.2018.
+//  Copyright © 2018 Aleksandr Nedorezov. All rights reserved.
+//
+
+#import "future21-3.h"
+
+@implementation future21_3
+
+@end
