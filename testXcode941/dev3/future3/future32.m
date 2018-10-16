9f61047855174b2db652a6f42eacd721f2ae0d4c
Aleksandr Nedorezov <n.sasha@list.ru>
2018-10-17 00:16:34 +0400

    commit future3-2

---
+++ testXcode941/dev3/future3/future32.m
@@ -0,0 +1,13 @@
+//
+//  future32.m
+//  testXcode941
+//
+//  Created by Aleksandr Nedorezov on 17.10.2018.
+//  Copyright © 2018 Aleksandr Nedorezov. All rights reserved.
+//
+
+#import "future32.h"
+
+@implementation future32
+
+@end
