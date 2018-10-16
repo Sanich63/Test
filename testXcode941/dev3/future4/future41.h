a494955b97aa799dfac7e0c7d439af558ee0bd44
Aleksandr Nedorezov <n.sasha@list.ru>
2018-10-17 00:15:27 +0400

    commit future4-1

---
+++ testXcode941/dev3/future4/future41.h
@@ -0,0 +1,13 @@
+//
+//  future41.h
+//  testXcode941
+//
+//  Created by Aleksandr Nedorezov on 17.10.2018.
+//  Copyright © 2018 Aleksandr Nedorezov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface future41 : NSObject
+
+@end
