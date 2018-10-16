97a99365eab56f2a15f73240c4fd8d44f0b5429e
Aleksandr Nedorezov <n.sasha@list.ru>
2018-10-16 23:43:25 +0400

    Future21-1

---
+++ testXcode941/Dev2/Future21/future21-1.h
@@ -0,0 +1,13 @@
+//
+//  future21-1.h
+//  testXcode941
+//
+//  Created by Aleksandr Nedorezov on 16.10.2018.
+//  Copyright © 2018 Aleksandr Nedorezov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface future21_1 : NSObject
+
+@end
