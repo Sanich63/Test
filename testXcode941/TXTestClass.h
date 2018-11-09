4ebb0fa25561108b50b04ad2c18f9ece8b29a38b
Aleksandr Nedorezov <n.sasha@list.ru>
2018-11-09 21:18:56 +0400

    Add initiate TXTestClass

---
+++ testXcode941/TXTestClass.h
@@ -0,0 +1,13 @@
+//
+//  TXTestClass.h
+//  testXcode941
+//
+//  Created by Aleksandr Nedorezov on 09.11.2018.
+//  Copyright © 2018 Aleksandr Nedorezov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TXTestClass : NSObject
+
+@end
