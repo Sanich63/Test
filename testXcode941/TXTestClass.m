5beb636d0c2496cc0e364bdfdbe1f2290eb36716
Aleksandr Nedorezov <n.sasha@list.ru>
2018-11-09 21:26:58 +0400

    Some dev-test-future-2 releasation

--- testXcode941/TXTestClass.m
+++ testXcode941/TXTestClass.m
@@ -10,4 +10,25 @@
 
 @implementation TXTestClass
 
+- (void)devTestFuture2Method1 {
+    NSLog(@"TXTestClass:devTestFuture2Method1");
+}
+
+- (id)devTestSomeTestClassMethod:(id)someTestArgument {
+    NSLog(@"TXTestClass:someTestClassMethod:someTestArgument - %@", someTestArgument);
+    
+    id someTestResult;
+    if ([someTestArgument isKindOfClass:[NSString class]] &&
+        someTestArgument != (NSString *)[NSNull class]) {
+        someTestResult = @([(NSString *)someTestArgument boolValue]);
+    } else {
+        someTestResult = @(NO);
+    }
+    return someTestResult;
+}
+
+- (void)devTestFuture2Method2 {
+    NSLog(@"TXTestClass:devTestFuture2Method2");
+}
+
 @end
